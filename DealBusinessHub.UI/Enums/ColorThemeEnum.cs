5283ba170dc920084cdcafdd23c058233931ecfc
Cientista-Avogadro <cientistaavogadro2019@gmail.com>
2024-05-09 15:44:02 +0100

    init: project creation | page: Auth login

---
+++ DealBusinessHub.UI/Enums/ColorThemeEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace DealBusinessHub.UI.Enums
+{
+    public static class ColorThemeEnum
+    {
+        public static readonly string RedPrimary = "#AC1C1C";
+        public static readonly string GreenPrimary = "#01C063";
+        public static readonly string RedSecondary = "#F0E2E2";
+        public static readonly string WhitePrimary = "#FFFFFF";
+        public static readonly string WhiteSecondary = "#F8F8F8";
+        public static readonly string GreyPrimary = "#3C3C3C";
+        public static readonly string GreySecondary = "#DADADA";
+        public static readonly string GreyTertiary = "#ECECEC";
+    }
+}
---
+++ DealBusinessHub.UI/Services/ThemeService.cs
@@ -0,0 +1,20 @@
+﻿namespace DealBusinessHub.UI.Services
+{
+
+    public class ThemeService
+    {
+        public event Action OnThemeChanged;
+        private string _currentTheme = "light";
+
+        public string CurrentTheme
+        {
+            get => _currentTheme;
+            set
+            {
+                _currentTheme = value;
+                OnThemeChanged?.Invoke();
+            }
+        }
+
+    }
+}
