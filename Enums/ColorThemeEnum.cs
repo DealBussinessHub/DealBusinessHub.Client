8fec7c0f70074ad0b9d0a9d0e569d383192af182
Cientista-Avogadro <cientistaavogadro2019@gmail.com>
2024-06-23 08:06:16 +0100

    Feat: DashBoard - Listings - Search

--- Enums/ColorThemeEnum.cs
+++ Enums/ColorThemeEnum.cs
@@ -7,6 +7,7 @@
         public static readonly string RedSecondary = "#F0E2E2";
         public static readonly string WhitePrimary = "#FFFFFF";
         public static readonly string WhiteSecondary = "#F8F8F8";
+        public static readonly string WhiteTertiary = "#F0F0F0";
         public static readonly string GreyPrimary = "#3C3C3C";
         public static readonly string GreySecondary = "#DADADA";
         public static readonly string GreyTertiary = "#ECECEC";
